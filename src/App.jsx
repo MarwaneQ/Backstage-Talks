d08fa2186dd54206d343de59667541fcf77a61fd
MarwaneQ <merouane12345bensadallah@gmail.com>
2024-03-16 19:00:44 -0700

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./styles.css";
-import { useRef } from "react";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { motion, useScroll, useSpring, useTransform  } from "framer-motion";
+
+const colors = ['#FFBE00', '#FA6922', '#00C2B6', '#FFFFFF', '#FF608C', '#E30512', '#1939BE'];
 
 function useParallax(value, distance) {
   return useTransform(value, [0, 1], [-distance, distance]);
@@ -12,16 +14,17 @@ function Image({ id }) {
   const y = useParallax(scrollYProgress, 300);
 
   return (
-    <section >
-      <div ref={ref} >
-        <img src={`./assets/${id}.png`} alt="jj" />
+    <section>
+      <div ref={ref}>
+        <img src={`${id}.png`} alt="jj" />
       </div>
-       <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2> 
+      <h6>book {id}</h6>
     </section>
   );
 }
 
 export default function App() {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
   const { scrollYProgress } = useScroll();
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
@@ -29,9 +32,23 @@ export default function App() {
     restDelta: 0.001,
   });
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const nextColorIndex = Math.floor(scrollYProgress.get() * colors.length);
+      setCurrentColorIndex(nextColorIndex);
+      document.body.style.backgroundColor = colors[nextColorIndex];
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollYProgress]);
+
   return (
     <>
-      {[1, 2, 3, 4, 5].map((image) => (
+      {[1, 2, 3, 4, 5, 6, 7].map((image) => (
         <Image id={image} key={image} />
       ))}
       <motion.div className="progress" style={{ scaleX }} />
